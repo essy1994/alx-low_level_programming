0b8791cba4ea08c01308e5279409e672511ca105
essy1994 <esternamara@gmail.com>
2023-02-16 10:24:19 -0800

    fputs

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,11 @@
 #include <stdio.h>
-
 /**
-   * main - Entry point
-   *
-   * Return: Always 0 (Success)
- */
+ *  * main - Entry point
+ *   *
+ *    * Return: always 0
+ *     */
 int main(void)
 {
-	
-	/*String initialization*/
-
-	char my_str[] = "\"Programming is like building a multilingual puzzle";
-	puts(my_str);
-	return (0);
+		puts("\"Programming is like building a multilingual puzzle");
+		return (0);
 }
