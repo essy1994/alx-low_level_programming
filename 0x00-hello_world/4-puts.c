02945dcf8d1b3f99ea4a3c0fb814c550a3511ed2
essy1994 <esternamara@gmail.com>
2023-02-16 10:09:17 -0800

    fputs

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * C program for printing content to the stdout 
+ */
+int main(void)
+{
+	//String initialization
+	char my_str[] = "Programming is like building a multilingual puzzle\n";
+	puts(my_str);
+	return 0;
+}
