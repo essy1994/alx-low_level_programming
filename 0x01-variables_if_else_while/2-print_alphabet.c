1d1d4632d98d532377359106c65864bb4a0a4b91
essy1994 <esternamara@gmail.com>
2023-02-17 10:10:53 -0800

    Use putchars

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *  *
+ *   * Return:always return 0
+ *    *
+ **/
+int main(void)
+{
+	char characters = "I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game";
+	putchar(characters);
+	return (0);
+}
