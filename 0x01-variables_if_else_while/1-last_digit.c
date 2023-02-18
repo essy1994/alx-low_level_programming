e101656b5013e9be2609b632e188afab1a3d5788
essy1994 <esternamara@gmail.com>
2023-02-18 15:05:31 -0800

    Last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ int main(void)
 	int_str = malloc(str_len + 1);
 	snprintf(int_str, str_len + 1, "%d", n);
 	last_digit = strlen(int_str) - 1;
-	if ((int)int_str[last_digit] > 5)
+	if ((n > 0) && (int)int_str[last_digit] > 5)
 	{
 		printf("Last digit of " "%d " "is " "%c " "and is greater than 5\n", n, int_str[last_digit]);
         }
@@ -32,7 +32,7 @@ int main(void)
 	{ 
 		printf("Last digit of " "%d " "is " "%c " "and is less than 6 and not 0\n", n, int_str[last_digit]); 
 	}
-	else if(n <0)
+	else if(n < 0)
 		printf("Last digit of " "%d " "is " "-(%c) " "and is less than 6 and not 0\n", n, int_str[last_digit]);
        return (0);
 
