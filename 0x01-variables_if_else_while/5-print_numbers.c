6fb74e3a92ceef97a629e8304cf22dad44b295ea
essy1994 <esternamara@gmail.com>
2023-02-18 15:54:52 -0800

    Print numbers 1 to 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *  *  * main - entry point
+ *   *   *
+ *    *    * Return: always returns 0
+ *     *     *
+ **/
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+	{
+		printf("%d", number);
+        }
+	putchar('\n');
+	return (0);
+}
