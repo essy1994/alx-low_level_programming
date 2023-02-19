866dd460fc623d7bb8cef2d4e9ad815bf6226124
essy1994 <esternamara@gmail.com>
2023-02-18 16:06:51 -0800

    Print numbers 1 to 10 using putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ *  *  * main - entry point
+ *   *   *
+ *    *    * Return: always returns 0
+ *     *     *
+ **/
+int main(void)
+{
+	int number;
+	for (number = 0; number < 10; number++)
+	{
+		putchar('0' + number);
+	}
+	putchar('\n');
+	return (0);
+}
