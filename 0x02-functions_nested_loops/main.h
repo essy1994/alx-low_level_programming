4845ab9716cb03a276cbc883fff6e9da18879b3a
essy1994 <esternamara@gmail.com>
2023-02-21 08:42:50 -0800

    Display characters using putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,10 +5,10 @@
  *   * @c: parameter to compare
  *    * Return: 1 or 0
  **/
-static inline int _putchar(char c);
+int _putchar(char c);
 /**
  *  * print_alphabet - function to print alphabet
  *   *
  **/
-static inline void print_alphabet(void);
+void print_alphabet(void);
 #endif
