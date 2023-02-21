40b9d019cf76302918d7a9ff4cd36e961b32783f
essy1994 <esternamara@gmail.com>
2023-02-21 13:16:31 -0800

    Disply characters

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+ *  *  *  * main - entry point
+ *   *   *   *
+ *    *    *    * Return: always returns 0
+ *     *     *     *
+ **/
+int _putchar(char c)
+{
+        write(1, &c, 1);
+        return (0);
+}
