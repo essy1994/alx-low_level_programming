6a6b681142a9ca4f07aa7110dc35d0d6685dbcc7
essy1994 <esternamara@gmail.com>
2023-02-21 10:50:40 -0800

    Displaying characters

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,11 +1,16 @@
 #ifndef _MAIN_H
 #define _MAIN_H
+#include <unistd.h>
 /**
  *  *  * _putchar - writes the character c to std
  *   *   * @c: parameter to compare
  *    *    * Return: 1 or 0
  **/
-int _putchar(char c);
+static int _putchar(char c)
+{
+        write(1, &c, 1);
+        return (0);
+}
 /**
  *  *  * print_alphabet - function to print alphabet
  *   *   *
