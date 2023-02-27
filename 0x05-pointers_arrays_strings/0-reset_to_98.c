516d201233a6995b7d006acf84de7c35c906a5d1
essy1994 <esternamara@gmail.com>
2023-02-27 04:54:51 -0800

    Reset 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *  *  *  * main - entry point
+ *   *   *   *
+ *    *    *    * Return: always returns 0
+ *     *     *     *
+**/
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,9 @@
+#ifndef _MAIN_H
+#define _MAIN_H
+/**
+ *  *  *  * _putchar - writes the character c to std
+ *   *   *   * @c: parameter to compare
+ *    *    *    * Return: 1 or 0
+**/
+void reset_to_98(int *n);
+#endif
