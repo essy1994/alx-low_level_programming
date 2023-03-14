c56a882d859df63e11a8b2a72a47eb4733552337
essy1994 <esternamara@gmail.com>
2023-03-14 08:42:44 -0700

    Corrections

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,7 +1,11 @@
 #include "main.h"
 #include <string.h>
+/**
+ *  * _strlen_recursion - length of string
+ *   * @s: string to count
+ *    * Return: length of string
+ **/
 int _strlen_recursion(char *s)
 {
-	return(strlen(s));
- 
+	return (strlen(s)); 
 }
--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,25 +1,26 @@
 #include "main.h"
 #include <math.h>
-int sqrt_helper (int n, int start, int end)
+int sqrt_helper(int n, int start, int end)
 {
-  int mid = (start + end) / 2;
-  int result = mid * mid;
-  /* Base case */
-  if (start > end)
-    return -1;
-  if (result == n)              /* If n has a natural square root */
-    return mid;
+   int mid = (start + end) / 2;
+   int result = mid * mid;
 
-  if (result < n)
-    return sqrt_helper (n, mid + 1, end);
-  else
-    return sqrt_helper (n, start, mid - 1);
+  /* Base case */
+   if (start > end)
+     return (-1);
+   if (result == n)              /* If n has a natural square root */
+     return (mid);
+
+   if (result < n)
+     return (sqrt_helper(n, mid + 1, end));
+   else
+     return (sqrt_helper(n, start, mid - 1));
 }
 
-int _sqrt_recursion (int n)
+int _sqrt_recursion(int n)
 {
-  if (n < 0)                    /* If n is negative, return -1 */
-    return -1;
+   if (n < 0)                    /* If n is negative, return -1 */
+     return (-1);
 
-  return sqrt_helper (n, 0, n);
+   return (sqrt_helper(n, 0, n));
 }
