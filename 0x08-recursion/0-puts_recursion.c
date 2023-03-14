dedc57139724d5cb9671711ebeb5aeb0bac98953
essy1994 <esternamara@gmail.com>
2023-03-14 05:29:42 -0700

    prints a string, followed by a new line

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,13 +3,13 @@
  *  * _puts_recursion - uses recursion
  *   * @s: string to recurse through
 **/
-void _puts_recursion (char *s)
+void _puts_recursion(char *s)
 {
   if (*s)
-    {
-      _putchar (*s);
-      _puts_recursion (++s);
-    }
+  {
+     _putchar (*s);
+     _puts_recursion (++s);
+  }
   else
     _putchar ('\n');
 }
